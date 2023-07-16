6ce8051d1d589a349def0e44afa1a28f852e8446
Shlombo <thomaszac28@aol.com>
2023-07-16 13:51:14 -0400

    List deletes objects

--- Assets/Scripts/HiddenObject.cs
+++ Assets/Scripts/HiddenObject.cs
@@ -14,7 +14,10 @@ public class HiddenObject : MonoBehaviour
     private void OnMouseDown()
     {
         /** If you want object to remain in list */
-        gameObject.SetActive(false);
+        // gameObject.SetActive(false);
+        HiddenManager.Instance.hiddenObjects.Remove(gameObject);
+        
+        DestroyImmediate(gameObject);
 
     }
     // Update is called once per frame
